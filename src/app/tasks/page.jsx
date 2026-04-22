e1e55586c2177ba8eb5ad50224599d05608786d2
Saharier Omi <gsaharier761@gmail.com>
2026-04-22 15:34:36 +0600

    add modal

--- src/app/tasks/page.jsx
+++ src/app/tasks/page.jsx
@@ -1,3 +1,4 @@
+import { AddTask } from "@/components/AddTask";
 import TasksCard from "@/components/TasksCard";
 import { getTasks } from "@/lib/task";
 import React from "react";
@@ -5,8 +6,9 @@ import React from "react";
 const TasksPage = async () => {
   const tasks = await getTasks();
   return (
-    <div>
-      <div className="max-w-6xl py-10 mx-auto grid grid-cols-2 md:grid-cols-3 gap-4">
+    <div className="max-w-6xl mx-auto py-10">
+      <AddTask />
+      <div className=" pt-8 mx-auto grid grid-cols-2 md:grid-cols-3 gap-4">
         {tasks.map((task) => (
           <TasksCard key={task.id} task={task} />
         ))}
